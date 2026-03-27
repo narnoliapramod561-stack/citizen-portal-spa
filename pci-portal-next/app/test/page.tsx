010ea127d1f47c424f3c4659d3f663ba30917015
Pramod Narnolia <narnoliapramod561@gmail.com>
2026-03-27 23:50:35 +0530

    fix(deploy): add test route to verify routing success

---
+++ pci-portal-next/app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div className="p-20 text-white">
+      <h1>Deployment Test - CSS/JS is working!</h1>
+      <p>If you see this, the routing is working but the root page might have SSR issues.</p>
+    </div>
+  );
+}
